9b2d04e47d4123f6764242f89fe04c71c1834da3
Andrea Ambu <andreambu@gmail.com>
2024-09-06 00:05:33 +0100

    chore(release): publish

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,6 +3,15 @@
 set -ex
 
 DIRS=$(find ./packages -type d | sort -r)
+for dir in $DIRS
+do
+  if [ -f "$dir/package.json" ]; then
+    echo "Publishing $dir"
+    (cd $dir && npm publish --access public)
+  fi
+done
+
+DIRS=$(find ./packages/rsapi/npm -type d -maxdepth 1 | sort -r)
 
 for dir in $DIRS
 do
